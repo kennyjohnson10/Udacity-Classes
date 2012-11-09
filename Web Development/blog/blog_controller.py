7c41d87cea157f1628745097d7c006c7cffb80b1
Mauro Oviedo <mauro.a.oviedo@gmail.com>
2012-11-09 12:39:45 -0500

    Added more method to the Handler class.

--- Web Development/blog/blog_controller.py
+++ Web Development/blog/blog_controller.py
@@ -39,6 +39,37 @@ class Handler(webapp2.RequestHandler):
 	def login_status(self):
 		return self.request.cookies.get('user_id')
 
+	def render_json(self, d):
+		json_txt = json.dumps(d)
+		self.response.headers['Content-Type'] = 'application/json; charset=UTF-8'
+		self.write(json_txt)
+
+	def set_secure_cookie(self, name, val):
+		cookie_val = make_secure_val(val)
+		self.response.headers.add_header(
+			'Set-Cookie',
+			'%s=%s; Path=/' % (name, cookie_val))
+
+	def read_secure_cookie(self, name):
+		cookie_val = self.request.cookies.get(name)
+		return cookie_val and check_secure_val(cookie_val)
+
+	def login(self, user):
+		self.set_secure_cookie('user_id', str(user.key().id()))
+
+	def logout(self):
+		self.response.headers.add_header('Set-Cookie', 'user_id=; Path=/')
+
+	def initialize(self, *a, **kw):
+		webapp2.RequestHandler.initialize(self, *a, **kw)
+		uid = self.read_secure_cookie('user_id')
+		self.user = uid and User.by_id(int(uid))
+
+		if self.request.url.endswith('.json'):
+			self.format = 'json'
+		else:
+			self.format = 'html'
+
 
 class BlogHandler(Handler):
 	def get(self):
