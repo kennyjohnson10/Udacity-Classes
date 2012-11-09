e4fb5d3b63b1899734f8946b2d80e5e397bd0827
Mauro Oviedo <mauro.a.oviedo@gmail.com>
2012-11-09 12:59:55 -0500

    Fixed python indentations for encryption.py.

--- Web Development/blog/helpers/encryption.py
+++ Web Development/blog/helpers/encryption.py
@@ -4,28 +4,29 @@ import hashlib
  
 
 def make_salt(length = 5):
-    return ''.join(random.choice(string.letters) for x in xrange(length))
+	return ''.join(random.choice(string.letters) for x in xrange(length))
 
 
 def make_pw_hash(name, pw, salt=None):
-    if not salt:
-        salt = make_salt()
+	if not salt:
+		salt = make_salt()
 
-    h = hashlib.sha256(name + pw + salt).hexdigest()
-    return '%s|%s' % (h, salt)
+	h = hashlib.sha256(name + pw + salt).hexdigest()
+	return '%s|%s' % (h, salt)
 
 
 def valid_pw(name, pw, h):
-    salt = h.split('|')[1]
-    return make_pw_hash(name, pw, salt) == h
+	salt = h.split('|')[1]
+	return make_pw_hash(name, pw, salt) == h
 
 
 def make_user_cookie_hash(user_id, name, salt=None):
-    if not salt:
-        salt = make_salt()
+	if not salt:
+		salt = make_salt()
 
-    h = hashlib.sha256(user_id + name + salt).hexdigest()
-    return '%s|%s|%s' % (user_id, h, salt)
+	h = hashlib.sha256(user_id + name + salt).hexdigest()
+	return '%s|%s|%s' % (user_id, h, salt)
+	
 
 def validate_user_cookie(user_id, name, h):
 	salt = h.split('|')[2]
