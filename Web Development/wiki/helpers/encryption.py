496f2631fba70a7ce995ec85e14e72f5cba74652
Mauro Oviedo <mauro.a.oviedo@gmail.com>
2012-11-17 18:43:49 -0500

    Got rid of get_secret function.

--- Web Development/wiki/helpers/encryption.py
+++ Web Development/wiki/helpers/encryption.py
@@ -1,14 +1,12 @@
 import hmac
 
-SECRET = 'Thanh is crazy'
+secret = 'Thanh is crazy'
 
-def get_secret():
-    return SECRET
 
 def make_secure_val(val):
-    return '%s|%s' % (val, hmac.new(secret, val).hexdigest())
+	return '%s|%s' % (val, hmac.new(secret, val).hexdigest())
 
 def check_secure_val(secure_val):
-    val = secure_val.split('|')[0]
-    if secure_val == make_secure_val(val):
-        return val
+	val = secure_val.split('|')[0]
+	if secure_val == make_secure_val(val):
+		return val
