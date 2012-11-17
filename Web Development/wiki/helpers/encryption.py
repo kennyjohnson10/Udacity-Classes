da3bc1c7b296a7689f7e39ab8931683c4436f931
Mauro Oviedo <mauro.a.oviedo@gmail.com>
2012-11-17 18:37:30 -0500

    Updated encryption function using Hmac library.

--- Web Development/wiki/helpers/encryption.py
+++ Web Development/wiki/helpers/encryption.py
@@ -1,32 +1,14 @@
-import random
-import string
-import hashlib
- 
+import hmac
 
-def make_salt():
-    return ''.join(random.choice(string.letters) for x in xrange(5))
+SECRET = 'Thanh is crazy'
 
+def get_secret():
+    return SECRET
 
-def make_pw_hash(name, pw, salt=None):
-    if not salt:
-        salt = make_salt()
+def make_secure_val(val):
+    return '%s|%s' % (val, hmac.new(secret, val).hexdigest())
 
-    h = hashlib.sha256(name + pw + salt).hexdigest()
-    return '%s|%s' % (h, salt)
-
-
-def valid_pw(name, pw, h):
-    salt = h.split('|')[1]
-    return make_pw_hash(name, pw, salt) == h
-
-
-def make_user_cookie_hash(user_id, name, salt=None):
-    if not salt:
-        salt = make_salt()
-
-    h = hashlib.sha256(user_id + name + salt).hexdigest()
-    return '%s|%s|%s' % (user_id, h, salt)
-
-def validate_user_cookie(user_id, name, h):
-	salt = h.split('|')[2]
-	return make_user_cookie_hash(user_id, name, salt) == h
+def check_secure_val(secure_val):
+    val = secure_val.split('|')[0]
+    if secure_val == make_secure_val(val):
+        return val
