2f995967227718e7d47602c99751961de1889437
Mauro Oviedo <mauro.a.oviedo@gmail.com>
2012-11-24 22:14:05 -0500

    Updated cace_helper function for better code reuse.

--- Web Development/wiki/helpers/cache_helper.py
+++ Web Development/wiki/helpers/cache_helper.py
@@ -1,75 +1,31 @@
 import logging
-import time
 
 #memcache
 from google.appengine.api import memcache
 
-#import models
-from blog_model import BlogPosts
+def cache_data(key, model_data):
+	#add time and values to memcache
+	memcache.set(key, model_data)
 
-#import appengine db methods for Model use
-from google.appengine.ext import db
+def get_cached_data(data_id, MODEL):
+	key = data_id
+	model_data = memcache.get(key)
 
-
-def get_latest_postings(update = False):
-
-	key = 'latest_postings'
-	blog_postings = memcache.get(key)
-
-	render_time_key = key + '_render_time'
-	render_time = memcache.get(render_time_key)
-
-	if blog_postings == None or update:
-		#show error in the console 
-		logging.error('DB POSTING QUERY')
-
-		blog_postings = db.GqlQuery("SELECT * "
-								"FROM BlogPosts "
-								"ORDER BY created DESC LIMIT 10")
-
-		#datastore query is executed
-		blog_postings = list(blog_postings)	
-
-		render_time = time.time()
-		reload_time = render_time		
-
-		#add time and values to memcache
-		memcache.set(key, blog_postings)
-		memcache.set(render_time_key, render_time)
-
-	else:
-		reload_time = time.time()
-	
-	render_time = int(reload_time - render_time)
-	return (blog_postings, render_time)
-
-def get_blog_post(post_id):
-	key = post_id
-	blog_post = memcache.get(key)
-
-	render_time_key = key + '_render_time'
-	render_time = memcache.get(render_time_key)
-
-	if blog_post == None:
+	if model_data == None:
 		#show error in the console 
 		logging.error('DB ID QUERY')
-		blog_post = BlogPosts.get_by_id(int(post_id))
 
 		#datastore query is executed
-		blog_post = blog_post			
+		model_data = MODEL.get_by_id(int(key))	
+		#model_data = model_data
 
 		render_time = time.time()
 		reload_time = render_time
 
 		#add time and values to memcache
-		memcache.set(key, blog_post)
-		memcache.set(render_time_key, render_time)
-
-	else:
-		reload_time = time.time()
+		memcache.set(key, model_data)
 	
-	render_time = int(reload_time - render_time)
-	return (blog_post, render_time)
+	return model_data
 
 def reset_cache():
 	memcache.flush_all()
